fbdf5dd6fbb70ebd3f9def6007c9d9763f21e454
PK De Ibiporã <112430622+ErikkSanderR@users.noreply.github.com>
2024-05-06 16:17:33 -0300

    Add files via upload

---
+++ sla.php
@@ -0,0 +1,26 @@
+<?php
+class Celular {
+    public $nome;
+    public $estilos;
+
+    function __construct($nome, $estilos) {
+        $this->nome = $nome;
+        $this->estilos = $estilos;
+    }
+
+    function get_nome() {
+        return $this->nome;
+    }
+
+    function get_cor() {
+        return $this->estilos;
+    }
+}
+
+$celular = new Celular("Celular", "Preto");
+    
+echo "O nome da marca do aparelho é: " . $celular->get_nome();
+
+echo"<br/>",
+
+echo "A cor do aparelho é: " . $celular->get_estilos();
